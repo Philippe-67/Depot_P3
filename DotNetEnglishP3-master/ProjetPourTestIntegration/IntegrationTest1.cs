7601e4dbb05bc4ab3df498a41914fb79e155c121
Philippe CHARLES <philippe.charles@axa.fr>
2023-03-16 17:39:01 +0100

    enregistrement de l'ensemble des tests

--- DotNetEnglishP3-master/ProjetPourTestIntegration/IntegrationTest1.cs
+++ DotNetEnglishP3-master/ProjetPourTestIntegration/IntegrationTest1.cs
@@ -11,6 +11,9 @@ using P3AddNewFunctionalityDotNetCore.Models.Repositories;
 using P3AddNewFunctionalityDotNetCore.Models.Services;
 using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
 using Xunit;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
@@ -23,7 +26,8 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void SaveNewProductToDbTest()
         {
             var options = new DbContextOptionsBuilder<P3Referential>()
-                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                 //UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                 .UseSqlServer("Server=.;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
                 .Options;
 
             var ctx = new P3Referential(options, _configuration);
@@ -33,75 +37,50 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
             var orderRepository = new OrderRepository(ctx);
             var languageService = new LanguageService();
 
-            ProductController productController = null;
+            //ProductController productController = null;
 
-            productController = new ProductController(new ProductService(cart, productRepository, orderRepository, _localizer), languageService);
+            ProductController productController = new ProductController(new ProductService(cart, productRepository, orderRepository, _localizer), languageService);
+            // act 
+            var product = new ProductViewModel { Name = "Lucien", Price = "100", Stock = "12", Details = "info", Description = "pp" };
 
-            Assert.NotNull(productController);
+            var result = productController.Create(product) as ViewResult;
+
+            Assert.Null(result);
+            // Vérifier que le résultat est nul
+            Assert.Null(result);
+
         }
 
-        [Fact]
-        public void GetProductByIdTest()
+        //[Fact]
+        public void DeleteProductFromDbTest()
         {
-            var cart = new Models.Cart();
+            // Setup
             var options = new DbContextOptionsBuilder<P3Referential>()
-                //.UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .UseSqlServer("Server=.;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
                 .Options;
-            ProductService productService = null;
-            Product product = null;
 
-            using (var ctx = new P3Referential(options, _configuration))
-            {
-                productService = new ProductService(cart,
-                    new ProductRepository(ctx),
-                    new OrderRepository(ctx), _localizer);
-                product = productService.GetProductById(1);
-            }
+            var ctx = new P3Referential(options, _configuration);
 
-            Assert.NotNull(product);
-            Assert.Equal(1, product.Id);
+            var cart = new Cart();
+            var productRepository = new ProductRepository(ctx);
+            var orderRepository = new OrderRepository(ctx);
+            var languageService = new LanguageService();
+
+            ProductController productController = new ProductController(new ProductService(cart, productRepository, orderRepository, _localizer), languageService);
+            ProductService productService = new ProductService(cart, productRepository, orderRepository, _localizer);
+
+
+            var totalProduct = productService.GetAllProducts();
+            var lastId = totalProduct.Max(p => p.Id);
+            productController.DeleteProduct(lastId);
+
+            // Assert
+            Assert.DoesNotContain(productRepository.GetAllProducts(), p => p.Id == lastId);
+
         }
 
-        [Fact]
-        public void DeleteProductFromDbAndRemoveFromCartTest()
-        {
-            var cart = new Models.Cart();
-            var options = new DbContextOptionsBuilder<P3Referential>()
-                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
-                .Options;
-            ProductService productService = null;
-            ProductViewModel newProduct = new ProductViewModel
-            {
-                Name = "UnDroleDeNom",
-                Price = "1000",
-                Description = "UneDroleDeChose",
-                Stock = "1",
-                Details = "VraimentDrole"
-            };
 
-            var product = new Product();
-
-            using (var ctx = new P3Referential(options, _configuration))
-            {
-                productService = new ProductService(cart,
-                    new ProductRepository(ctx),
-                    new OrderRepository(ctx), _localizer);
-                productService.SaveProduct(newProduct);
-
-                product = ctx.Product.FirstOrDefault(p => p.Name == "UnDroleDeNom");
-                Assert.NotNull(product);
-
-                cart.AddItem(product, 3);
-                Assert.NotEmpty(cart.Lines);
-
-                productService.DeleteProduct(product.Id);
-                product = productService.GetProductById(product.Id);
-            }
-
-            Assert.Null(product);
-            Assert.Empty(cart.Lines);
-        }
     }
 }
+
 
